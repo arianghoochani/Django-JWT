0d848c2c71cf077420f4d1650722c8eaa6c794e9
arian <arian_gh22@yahoo.com>
2024-01-17 12:00:12 +0330

    creaate registeruserrequest class

--- jwt/authorizationApp/views.py
+++ jwt/authorizationApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError 
 from .models import CustomUser
 from .classes import AuthorizationException,AuthorizingResponse
 from .serializers import AuthorizingResponseSerializer
@@ -39,6 +40,35 @@ def getUser(exception,token):
             logging.warning("error={error} exceptionCode={code} desc={desc}".format(error="entered token is invalid", code=exception.read().exceptionCode, desc=exception.exceptionDesc)) 
     return user
 
+def verifyRegisterUserRequest(exception, request):
+    data = request.data
+    registerUserRequest = "" 
+    authorization_header = request.headers.get('Authorization')
+    token_type, token = authorization_header.split(' ')
+    user = getUser(exception,token)
+    if (token_type.lower() == 'bearer'):
+        if (user.is_superuser):
+            try:
+                serializer = RegisterUserRequestSerializer(data=data)
+                serializer.is_valid(raise_exception=True)
+                registerUserRequest = serializer.save()
+            except ValidationError :
+                attributeName = list(serializer.errors.keys())[0]
+                exception.errorKey = attributeName
+                try:
+                    logging.info("error={error} value={value}".format(error=str(ValidationError), value=data[attributeName]))
+                except:
+                    logging.warning("error={error}  {attribute} is empty".format(error=str(ValidationError), attribute = attributeName))
+            except Exception as unknownException :
+                exception.errorKey = "system_error"
+                logging.error("error={error} exceptionCode={code} desc={desc}".format(error=unknownException, code=exception.read().exceptionCode, desc=exception.exceptionDesc))
+        else:
+            exception.errorKey = "inadequate_credentials"
+    else:
+        exception.errorKey = "invalid_tokenType_error"
+    return registerUserRequest
+
+
 @api_view(['POST'])
 def authorizing(request):
     exception = AuthorizationException(errorKey="initial_error")
@@ -55,3 +85,37 @@ def authorizing(request):
     serializer = AuthorizingResponseSerializer(authorizingResponse)
 
     return Response(serializer.data)
+
+
+
+
+
+@api_view(['POST'])  
+def registerUser(request):
+    returnFlag = "0"
+    exception = AuthorizationException(errorKey="initial_error")
+    registerUserRequest = verifyRegisterUserRequest(exception,request)
+    if (registerUserRequest):
+        try:
+            if CustomUser.objects.filter(username = registerUserRequest.username).exists():
+                exception.errorKey = "user_allready_exists"
+            else:
+                user = CustomUser(username = registerUserRequest.username,scope = registerUserRequest.scope,is_superuser = registerUserRequest.is_superuser)
+                user.set_password(registerUserRequest.password)
+                user.save(force_insert=True)
+                returnFlag = "1"
+                exception.errorKey = "successful"
+        except:
+            exception.errorKey = "failed_to_register_new_user"
+
+    exception.read()
+    registeruserResponse = RegisterUserResponse(exception,returnFlag)
+    serializer = RegisterUserResponseSerializer(registeruserResponse)
+    return Response(serializer.data)
+
+class RegisterUserRequest():
+    def __init__(self,username,password,scope,is_superuser):
+        self.username = username
+        self.password = password
+        self.scope = scope
+        self.is_superuser = is_superuser
