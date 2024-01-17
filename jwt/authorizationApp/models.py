ab88f564c40292ea67551d15bad4c99b2a5243e3
arian <arian_gh22@yahoo.com>
2024-01-17 11:08:54 +0330

    creatng custom user class

--- jwt/authorizationApp/models.py
+++ jwt/authorizationApp/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    scope = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.username
