9f9f88c09324141ead10f69c3e311b20d714d585
arian <arian_gh22@yahoo.com>
2024-01-17 12:10:10 +0330

    fixing urls

--- jwt/authorizationApp/urls.py
+++ jwt/authorizationApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns  = [
-    path('verifytoken/',views.login,name="login"),
+    path('verifytoken/',views.authorizing,name="authorizing"),
     path('register/',views.registerUser,name="registerUser"),
     ]
