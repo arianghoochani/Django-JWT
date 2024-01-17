d846de317170c57067f116e72e208bb99145eefd
arian <arian_gh22@yahoo.com>
2024-01-17 10:57:38 +0330

    adding base urls of the project

--- jwt/jwt/urls.py
+++ jwt/jwt/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('jwt/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt/',include('authorizationApp.urls')),
 ]
+
