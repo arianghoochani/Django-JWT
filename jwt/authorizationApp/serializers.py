b699fb1b196e730a2d44b3290eb7d4304c88c0b6
arian <arian_gh22@yahoo.com>
2024-01-17 12:02:35 +0330

    creaate registeruserrequest and registeruserresponse serializers

--- jwt/authorizationApp/serializers.py
+++ jwt/authorizationApp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .classes import RegisterUserRequest
 
 class AuthorizingResponseSerializer(serializers.Serializer):
     status = serializers.CharField()
@@ -6,3 +7,15 @@ class AuthorizingResponseSerializer(serializers.Serializer):
     username = serializers.CharField()
     scope = serializers.CharField()
 
+class RegisterUserRequestSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length = 30)
+    password = serializers.CharField(min_length = 8)
+    scope = serializers.CharField()
+    is_superuser = serializers.CharField(max_length=1,min_length = 1)
+
+    def create(self, validated_data):
+        return RegisterUserRequest(**validated_data)
+    
+class RegisterUserResponseSerializer(serializers.Serializer):
+    code = serializers.CharField()
+    message = serializers.CharField()
