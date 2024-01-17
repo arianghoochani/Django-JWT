dbab7f5c632c7aaf91b6613ca2ec42a4fcb986e8
arian <arian_gh22@yahoo.com>
2024-01-17 12:49:50 +0330

    modify

--- jwtService/authorizationApp/exception.py
+++ jwtService/authorizationApp/exception.py
@@ -8,6 +8,6 @@ from rest_framework.response import Response
 
 def CustomException(exc, context):
     if isinstance(exc, AuthenticationFailed):
-        return Response({'status':"0",'description':"failed to authorize.",'username':"",'scope':""}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'description':"failed to authorize.",'username':"",'scope':""}, status=status.HTTP_401_UNAUTHORIZED)
     return exception_handler(exc, context)
 
