015e5f89abe2a90e4fb5eac4e368e65a7e2b74aa
arian <arian_gh22@yahoo.com>
2024-01-17 10:50:36 +0330

    adding required installed apps to the settings

--- jwt/jwt/settings.py
+++ jwt/jwt/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authorizationApp',
+    'rest_framework_simplejwt',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
