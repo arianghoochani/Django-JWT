d9f049eeae3777a1a3ec6967a8f799efd48e631c
arian <arian_gh22@yahoo.com>
2024-01-17 12:36:05 +0330

    removing the extra directory

--- jwt-project/authorizationApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthorizationappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authorizationApp'
--- jwt-project/authorizationApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns  = [
-    path('verifytoken/',views.authorizing,name="authorizing"),
-    path('register/',views.registerUser,name="registerUser"),
-    ]
